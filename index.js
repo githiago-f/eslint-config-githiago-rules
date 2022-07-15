a5e3e88ddc30de57ba507239180f15c6d85f3902
Thiago Farias <52945005+githiago-f@users.noreply.github.com>
2022-07-15 19:01:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ module.exports = {
     "eol-last": ["error", "always"],
     "newline-per-chained-call": ["error", { "ignoreChainWithDepth": 2 }],
     "no-mixed-spaces-and-tabs": "error",
-    "object-curly-newline": ["error", { "multiline": true }]
+    "object-curly-newline": ["error", { "multiline": true }],
+    "import/no-default-export": "error"
   }
 }
